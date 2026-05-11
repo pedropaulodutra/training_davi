76de35dd3462d7caa213e5809774d2e9e446a5d2
Davi <obredcusstodio@gmail.com>
2026-05-11 14:22:30 -0300

    fiz o calculo do imc

--- imc.py
+++ imc.py
@@ -2,3 +2,6 @@ altura = float(input('Digite a sua altura: '))
 peso = float(input('Digite o seu peso: '))
 
 # Davi, faca calculo aqui do imc
+
+soma = peso/altura**2
+print(f'o seu imc e igual a  {soma:.2f}')
