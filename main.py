7ed63ed7300625e243b1f083ac2603ba2b67a2ca
Pedro Dutra <pedro.dutra@muvz.ai>
2026-05-12 13:00:47 -0300

    feat: add students dictionary to davi study

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-# NAo e assim davi, faca de outro jeito
-nome = "davi"
+students = {
+    'Maria', [7.5, 5.0, 6.5],
+    'Kathleen', [9.1, 2.5, 6.4],
+    'Pedro', [1.0, 10.0, 9.6],
+    'Thiago', [8.7, 6.4, 8.7]
+}
