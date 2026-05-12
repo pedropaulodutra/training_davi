8c26aec9413980514c05b85d08975383d7659fb7
Davi <obredcusstodio@gmail.com>
2026-05-12 15:12:47 -0300

    exercicio novo

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-alunos = {
-    "Pedro": [8.5, 7.0, 9.5],
-    "Maria": [10.0, 9.0, 8.5],
-    "João": [6.0, 7.5, 5.5],
-    "Ana": [9.0, 9.5, 10.0]
-}
+saldo = 0 
+
+while True:
+    opcao = input(''' 
+[S]acar
+[D]epositar
+[E]xtrato
+[Q]uit
+                  
+''')
+    if opcao.upper() == 'S':
+        valor = float(input('digite o valor que quer sacar'))
+        if saldo < valor:
+            print('nao tem dinheiro para sacar')
+            continue
+        saldo = saldo -valor
+    elif opcao.upper() == 'D':
+        valor = float(input('digite o valor que deseja depositar')) 
+        saldo = saldo +valor 
+    elif opcao.upper() == 'E':
+        print(f'seu saldo e de R${saldo}')
+    elif opcao.upper() == 'Q':
+        break
+    else:
+        print('escolha uma opcao valida ')
+        continue
